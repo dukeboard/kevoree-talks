6670fff3f9f54d11aba0645973e6d470f4464b49
François Fouquet <fouquet.f@gmail.com>
2012-06-08 14:07:30 +0200

    kevoree talks

---
+++ org.kevoree.talks.adam/src/main/java/org/kevoree/slides/adamSlides/FraSCAtiKevoree2012Dev.java
@@ -0,0 +1,18 @@
+package org.kevoree.slides.adamSlides;
+
+import org.kevoree.annotation.ComponentType;
+import org.kevoree.slides.framework.KevoreeSlidePageDev;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/04/12
+ * Time: 10:57
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@ComponentType
+public class FraSCAtiKevoree2012Dev extends KevoreeSlidePageDev {
+
+
+}
