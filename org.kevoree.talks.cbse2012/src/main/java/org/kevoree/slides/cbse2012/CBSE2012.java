ee296dd5f422c3d0f11547480eed382ad84e75bd
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-07-09 18:04:30 +0200

    add url to the paper

--- org.kevoree.talks.cbse2012/src/main/java/org/kevoree/slides/cbse2012/CBSE2012.java
+++ org.kevoree.talks.cbse2012/src/main/java/org/kevoree/slides/cbse2012/CBSE2012.java
@@ -1,6 +1,8 @@
 package org.kevoree.slides.cbse2012;
 
 import org.kevoree.annotation.ComponentType;
+import org.kevoree.annotation.DictionaryAttribute;
+import org.kevoree.annotation.DictionaryType;
 import org.kevoree.slides.framework.KevoreeSlidePage;
 
 /**
@@ -11,6 +13,7 @@ import org.kevoree.slides.framework.KevoreeSlidePage;
  * @author Erwan Daubert
  * @version 1.0
  */
+@DictionaryType(@DictionaryAttribute(name = "paperURL", defaultValue = "http://hal.inria.fr/hal-00713769/en", optional = false))
 @ComponentType
 public class CBSE2012 extends KevoreeSlidePage {
 }
