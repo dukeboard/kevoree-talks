5f268b596a7d006a323e057c79b1138289b574e5
François Fouquet <fouquet.f@gmail.com>
2012-09-18 18:01:46 +0200

    add brush

---
+++ org.kevoree.talks.snt2012/src/main/java/org/kevoree/slides/cbse2012/CBSE2012.java
@@ -0,0 +1,19 @@
+package org.kevoree.slides.cbse2012;
+
+import org.kevoree.annotation.ComponentType;
+import org.kevoree.annotation.DictionaryAttribute;
+import org.kevoree.annotation.DictionaryType;
+import org.kevoree.slides.framework.KevoreeSlidePage;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/04/12
+ * Time: 10:57
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@DictionaryType(@DictionaryAttribute(name = "paperURL", defaultValue = "http://hal.inria.fr/hal-00713769/en", optional = false))
+@ComponentType
+public class CBSE2012 extends KevoreeSlidePage {
+}
