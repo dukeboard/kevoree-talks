df1d336d94df8017b4bc1eef73fe3eb16ec558d9
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-09-18 17:57:12 +0200

    fix main value

--- org.kevoree.talks.framework/src/main/java/org/kevoree/slides/framework/KevoreeSlidePage.java
+++ org.kevoree.talks.framework/src/main/java/org/kevoree/slides/framework/KevoreeSlidePage.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 @Library(name = "KevoreeWeb")
 @ComponentType
 @DictionaryType({
-		@DictionaryAttribute(name = "main", defaultValue = "showcase.html"),
+		@DictionaryAttribute(name = "main", defaultValue = "index.html"),
 		@DictionaryAttribute(name = "wsurl", defaultValue = "ws://localhost:8092/keynote", optional = true),
 		@DictionaryAttribute(name = "paperURL", optional = false, defaultValue = "")
 })
