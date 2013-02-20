bd9e63414154272259bffa45eae3ad4abadbf321
Francois Fouquet <fouquet.f@gmail.com>
2013-02-20 08:39:32 +0100

    add EBRC / Duke PhD

---
+++ org.kevoree.talks.dukePhD/src/main/java/org/kevoree/slides/dukePhD/DukePhDDev.java
@@ -0,0 +1,8 @@
+package org.kevoree.slides.dukePhD;
+
+import org.kevoree.annotation.*;
+import org.kevoree.slides.framework.KevoreeSlidePageDev;
+
+@ComponentType
+public class DukePhDDev extends KevoreeSlidePageDev {
+}
