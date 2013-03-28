fc6a83486d8bfd76f56926520210fe531184b452
Francois Fouquet <fouquet.f@gmail.com>
2013-03-28 18:09:16 +0100

    add GDR GPL talk

---
+++ org.kevoree.talks.gdr2013/src/main/java/org/kevoree/slides/gdr2013/GDR2013.java
@@ -0,0 +1,11 @@
+package org.kevoree.slides.gdr2013;
+
+import org.kevoree.annotation.ComponentType;
+import org.kevoree.annotation.DictionaryAttribute;
+import org.kevoree.annotation.DictionaryType;
+import org.kevoree.slides.framework.KevoreeSlidePage;
+
+@DictionaryType(@DictionaryAttribute(name = "paperURL", defaultValue = "http://hal.inria.fr/hal-00713769/en", optional = false))
+@ComponentType
+public class GDR2013 extends KevoreeSlidePage {
+}
---
+++ org.kevoree.talks.gdr2013/src/main/java/org/kevoree/slides/gdr2013/GDR2013Dev.java
@@ -0,0 +1,8 @@
+package org.kevoree.slides.gdr2013;
+
+import org.kevoree.annotation.*;
+import org.kevoree.slides.framework.KevoreeSlidePageDev;
+
+@ComponentType
+public class GDR2013Dev extends KevoreeSlidePageDev {
+}
